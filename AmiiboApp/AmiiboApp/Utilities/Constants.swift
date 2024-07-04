4e24ca90b95433bee6c560113be849348e5e710c
manishparihar948 <manish.parihar948@gmail.com>
2024-07-04 10:10:21 +0200

    Added NetworkManager class

--- AmiiboApp/AmiiboApp/Utilities/Constants.swift
+++ AmiiboApp/AmiiboApp/Utilities/Constants.swift
@@ -47,6 +47,15 @@ enum Constants {
     /* Extras */
     static let placeholder = "amiiboPlaceholder"
     static let ok = "OK"
+    
+    /* Network Manager */
+    static let serverError = "Server Error"
+    static let invalidURL = "The URL is invalid"
+    static let invalidResponse = "There was an invalid response from the server. Please try again later"
+    static let invalidData = "There was an error connecting to the server. Please try again later"
+    static let unableToComplete = "Unable to complete your request at this time. Please check you internet connection"
+    
+    
 }
 
 enum SFSymbols {
